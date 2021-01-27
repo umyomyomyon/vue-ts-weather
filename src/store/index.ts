b2a63efa2de669b0de906f9612d33a32289a78d8
umyomyomyon <minecraft.sentry.yym@gmail.com>
2021-01-27 10:43:18 +0900

    vuex strictモード

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store<any>({
-  state: { hoge: 0 },
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: { hoge: 0 },
+    mutations: {},
+    actions: {},
+    modules: {},
+    // storeがmutation handler以外で変更されたらエラーを出す
+    strict: true
 });
